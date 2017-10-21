df611eafda8f45f1d24e8cf197d68a983891ded9
Sietse Ringers <mail@sietseringers.net>
2017-10-21 15:35:38 +0200

    Hopefully fix SIGPIPE crash

--- ios/IRMA/main.m
+++ ios/IRMA/main.m
@@ -12,6 +12,7 @@
 #import "AppDelegate.h"
 
 int main(int argc, char * argv[]) {
+  signal(SIGPIPE, SIG_IGN);
   @autoreleasepool {
     return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
   }
