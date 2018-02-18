f8ca3daddaf849b724d161aececc0c1ada0c5612
Confiks <confiks@scriptbase.org>
2018-02-18 19:31:09 +0100

    Fix up Teaser background images, add texts

--- components/NewEnrollment/children/Teaser.js
+++ components/NewEnrollment/children/Teaser.js
@@ -4,11 +4,8 @@ import { ImageBackground } from 'react-native';
 
 import {
   Button,
-  Container,
   Text,
-  Footer,
   Content,
-  Header,
   View,
 } from 'native-base';
 
@@ -25,7 +22,7 @@ export default class Teaser extends Component {
     dismissTeaser: PropTypes.func.isRequired,
   }
 
-  render() {
+  renderFooter() {
     const { dismissEnrollment, dismissTeaser } = this.props;
 
     const footerStyle = {
@@ -34,30 +31,81 @@ export default class Teaser extends Component {
       justifyContent: 'center',
     };
 
-    const body = (
-      <Container>
-        <Content />
-        <View {...footerStyle} style={{}}>
-          <Button info onPress={dismissEnrollment} style={{marginTop: 5, marginRight: 20}}>
-            <Text>Demo</Text>
-          </Button>
-          <Button info onPress={dismissTeaser} style={{marginTop: 5}}>
-            <Text>Register</Text>
-          </Button>
-        </View>
-      </Container>
+    return (
+      <View {...footerStyle} style={{}}>
+        <Button info onPress={dismissEnrollment} style={{marginTop: 5, marginRight: 20}}>
+          <Text>Demo</Text>
+        </Button>
+        <Button info onPress={dismissTeaser} style={{marginTop: 5}}>
+          <Text>Register</Text>
+        </Button>
+      </View>
     );
+  }
+
+  renderTexts(statement, quote) {
+    const statementStyle = {
+      color: '#305f91',
+      fontSize: 26,
+      fontWeight: '300',
+      marginTop: 40,
+      paddingHorizontal: 20,
+    };
+
+    const quoteStyle = {
+      color: '#ffffff',
+      fontFamily: 'Times New Roman',
+      fontSize: 30,
+      fontStyle: 'italic',
+      fontWeight: '800',
+      marginTop: 30,
+      paddingHorizontal: 20,
+      textAlign: 'right',
+    };
 
     return (
-      <Swiper showButtons={false}>
-        <ImageBackground style={{flex: 1}} source={passportImage}>
-          { body }
+      <Content>
+        <Text style={statementStyle}>{ statement }</Text>
+        <Text style={quoteStyle}>{ quote }</Text>
+      </Content>
+    );
+  }
+
+  render() {
+    return (
+      <Swiper>
+        <ImageBackground
+          imageStyle={{marginTop: 300}}
+          source={passportImage}
+          style={{flex: 1, backgroundColor: '#2dbfce'}}
+        >
+          { this.renderTexts(
+              'IRMA is like a digital passport on your phone. It puts you in control and keeps your personal details private and secure.',
+              '"I reveal my attributes"'
+          )}
+          { this.renderFooter() }
         </ImageBackground>
-        <ImageBackground style={{flex: 1}} source={shopImage}>
-          { body }
+        <ImageBackground
+          imageStyle={{marginTop: 100}}
+          source={shopImage}
+          style={{flex: 1, backgroundColor: '#abe3f4'}}
+        >
+          { this.renderTexts(
+              'Reveal what\'s relevant – and nothing more',
+              '"I\'m older than 18"'
+          )}
+          { this.renderFooter() }
         </ImageBackground>
-        <ImageBackground style={{flex: 1}} source={authImage}>
-          { body }
+        <ImageBackground
+          imageStyle={{marginTop: 100}}
+          source={authImage}
+          style={{flex: 1, backgroundColor: '#65b7cc'}}
+        >
+          { this.renderTexts(
+              'Prove it\'s you, gain access, forget passwords...',
+              '"It\'s me, log me in!"'
+          )}
+          { this.renderFooter() }
         </ImageBackground>
       </Swiper>
     );
