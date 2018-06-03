ac8a4b0620479a076a1bdb3b4ca70327def1c195
Confiks <confiks@scriptbase.org>
2018-06-03 22:34:25 +0200

    Ignore lifecycle deprecation warnings (mainly from libraries) and another bugged warning. Make clear store init only returns store.

--- components/Root/RootProvider.js
+++ components/Root/RootProvider.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
 import { Provider as ReduxProvider } from 'react-redux';
 import { Root as NBRoot, StyleProvider } from 'native-base';
+import { YellowBox } from 'react-native';
 
-import init from 'store/init';
+import initStore from 'store/init';
 import getTheme from 'lib/native-base-theme/components';
 
 import RootContainer from './RootContainer';
 
-const {
-  store
-} = init();
+// Ignore specific deprecation warnings or harmless issues
+if(__DEV__) {
+  YellowBox.ignoreWarnings([
+    // TODO: Migrate to (libraries with) new lifecycle methods, and remove this ignore
+    'Warning: isMounted(...) is deprecated',
+    // TODO: Remove when react-native#17679 released
+    'Module RCTImageLoader requires main queue setup',
+    'Module RNMail requires main queue setup',
+    // TODO: Remove when react-native#18201 is fixed
+    'Class RCTCxxModule was not exported',
+  ]);
+}
+
+// Initialize store once per application instance
+const store = initStore();
 
 export default class RootProvider extends Component {
   render() {
