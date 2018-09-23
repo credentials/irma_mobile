88a23b03012c357be3596225826f464aaafb8b23
Confiks <confiks@scriptbase.org>
2018-09-23 20:14:58 +0200

    fix: Only try to guess GOPATH in iOS build script when it is not already set. Closes #12.

--- ios/scripts/gomobile_bind.sh
+++ ios/scripts/gomobile_bind.sh
@@ -3,6 +3,11 @@ if [[ $go_version = *'go1.9.4'* ]]; then
   export CGO_CFLAGS_ALLOW='-fmodules|-fblocks'
 fi
 
-export GOPATH=`echo ~/go`
+# When no GOPATH is defined, try the user's homedir
+# This might allow builds from the XCode GUI without additional configuration
+if [ -z "$GOPATH" ]; then
+	export GOPATH=`echo ~/go`
+fi
+
 export PATH="$GOPATH/bin:$PATH"
 $GOPATH/bin/gomobile bind -target ios github.com/privacybydesign/irma_mobile/irmagobridge
