3a5d6b2f018fd64917c467cf6bd08a57daa2dcef
Confiks <confiks@scriptbase.org>
2019-01-25 16:34:03 +0100

    fix: Fix back button not working for navigation stacks.

--- android/app/src/main/java/foundation/privacybydesign/irmamobile/MainActivity.java
+++ android/app/src/main/java/foundation/privacybydesign/irmamobile/MainActivity.java
@@ -1,10 +1,13 @@
 package foundation.privacybydesign.irmamobile;
 
 import com.reactnativenavigation.NavigationActivity;
+import com.reactnativenavigation.utils.CommandListenerAdapter;
 
 public class MainActivity extends NavigationActivity {
   @Override
   public void invokeDefaultOnBackPressed() {
-      moveTaskToBack(true);
+      if (!navigator.handleBack(new CommandListenerAdapter())) {
+          super.onBackPressed();
+      }
   }
 }
