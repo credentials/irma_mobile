2d01e259d1ffa82ee745ec9dce24b84695ae5037
David Venhoek <david@venhoek.nl>
2019-03-12 16:19:25 +0100

    fix: Removed code from main activity that prevented back button from working properly.

--- android/app/src/main/java/foundation/privacybydesign/irmamobile/MainActivity.java
+++ android/app/src/main/java/foundation/privacybydesign/irmamobile/MainActivity.java
@@ -4,10 +4,4 @@ import com.reactnativenavigation.NavigationActivity;
 import com.reactnativenavigation.utils.CommandListenerAdapter;
 
 public class MainActivity extends NavigationActivity {
-  @Override
-  public void invokeDefaultOnBackPressed() {
-      if (!navigator.handleBack(new CommandListenerAdapter())) {
-          super.onBackPressed();
-      }
-  }
 }
