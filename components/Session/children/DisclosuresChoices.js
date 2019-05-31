a11d354700a7bd8b6fccdec35d2012419394b9f2
Sietse Ringers <mail@sietseringers.net>
2019-05-31 11:23:13 +0200

    refactor: simplify state and reuse styles in session children components

--- components/Session/children/DisclosuresChoices.js
+++ components/Session/children/DisclosuresChoices.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import _ from 'lodash';
 import PropTypes from 'prop-types';
 
-import { Dimensions, StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
 import {
   View,
   Text,
@@ -10,9 +10,9 @@ import {
 
 import Disjunction from './Disjunction';
 import { namespacedTranslation } from 'lib/i18n';
-import nbVariables from 'lib/native-base-theme/variables/platform';
 
-const screenWidth = Dimensions.get('window').width;
+import SessionStyles, { screenWidth } from './Styles';
+
 const t = namespacedTranslation('Session');
 
 export default class DisclosureChoices extends Component {
@@ -28,7 +28,7 @@ export default class DisclosureChoices extends Component {
   }
 
   renderDisjunction = ([candidateSets, choice], conjunctionIndex) => {
-    const { hideUnchosen, makeDisclosureChoice, session: { status, disclosuresLabels } } = this.props;
+    const { makeDisclosureChoice, session: { status, disclosuresLabels } } = this.props;
     const label = disclosuresLabels && disclosuresLabels[conjunctionIndex] ? disclosuresLabels[conjunctionIndex] : null;
 
     return (
@@ -36,34 +36,52 @@ export default class DisclosureChoices extends Component {
         key={conjunctionIndex}
         candidateSets={candidateSets}
         makeDisclosureChoice={status === 'requestPermission' ? j => makeDisclosureChoice(conjunctionIndex, j) : null}
-        hideUnchosen={hideUnchosen}
         choice={choice}
         label={label}
       />
     );
   }
 
-  renderSeparator = index => {
+  renderSeparator = (index, count) => {
+    if (index === count-1)
+      return null;
+
     return (
       <View key={`sep-${index}`} style={styles.separator}>
-        <View style={styles.borderBottom}></View>
+        <View style={{...SessionStyles.borderBottom, ...styles.borderBottom}}></View>
         <Text note style={styles.text}>{ t('.and') }</Text>
-        <View style={styles.borderBottom}></View>
+        <View style={{...SessionStyles.borderBottom, ...styles.borderBottom}}></View>
       </View>
     );
   }
+
+  // When showing disclosed attributes after the session, filter away all unchosen candidate sets
+  // from each outer conjunction
+  filterUnchosen = ([candidateSets, choice]) => (
+    !this.props.hideUnchosen ? [candidateSets, choice] : [[candidateSets[choice]], 0]
+  )
+
+  // When showing disclosed attributes after the session, filter away optional outer conjunctions
+  // that the user satisfied by sending nothing
+  filterEmpty = ([candidateSets, choice]) => !(
+    this.props.hideUnchosen && candidateSets[choice] && candidateSets[choice].length === 0
+  );
 
   render() {
     const { session } = this.props;
     const { disclosuresCandidates, disclosureIndices } = session;
 
-    const count = disclosuresCandidates.length;
+    const filtered = _
+      .zip(disclosuresCandidates, disclosureIndices)
+      .filter(this.filterEmpty)
+      .map(this.filterUnchosen);
+    const count = filtered.length;
 
     return (
       <View>
-        { _.zip(disclosuresCandidates, disclosureIndices)
+        { filtered
            .map(this.renderDisjunction)
-           .map((disjunction, index) => [disjunction, index === count-1 ? null : this.renderSeparator(index)])
+           .map((disjunction, index) => [disjunction, this.renderSeparator(index, count)])
         }
       </View>
     );
@@ -72,10 +90,7 @@ export default class DisclosureChoices extends Component {
 
 const styles = StyleSheet.create({
   borderBottom: {
-    borderBottomWidth: nbVariables.borderWidth,
-    borderColor: nbVariables.cardBorderColor,
-    marginBottom: 8,
-    width: (screenWidth/2)-60,
+    marginBottom: 8, width: (screenWidth/2)-60,
   },
   text: {
     paddingHorizontal: 20,
