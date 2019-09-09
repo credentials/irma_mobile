a9c040e46df433af2d2a550e2ffcac1d0972d82c
Confiks <confiks@scriptbase.org>
2019-09-09 08:34:26 +0200

    feat: Reintroduce Root container that agressively remounts navigators depending on the state of the app

---
+++ components/Root/RootContainer.js
@@ -0,0 +1,117 @@
+import React, { Component, PureComponent } from 'react';
+import { connect, Provider as ReduxProvider } from 'react-redux';
+import { Root as NBRoot, StyleProvider, View, Text } from 'native-base';
+import { createAppContainer } from 'react-navigation';
+
+import store from 'store';
+import services from 'store/services';
+import getTheme from 'lib/native-base-theme/components';
+
+import { AppUnlockNavigator, EnrollmentNavigator, MainNavigator } from './navigators';
+
+export default class RootProvider extends PureComponent {
+  render() {
+    return (
+      <ReduxProvider store={store}>
+        <RootContainer />
+      </ReduxProvider>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  const {
+    irmaConfiguration: {
+      loaded: configurationLoaded,
+    },
+    preferences: {
+      loaded: preferencesLoaded,
+    },
+    enrollment: {
+      loaded: enrollmentLoaded,
+      enrolledSchemeManagerIds,
+    },
+    credentials: {
+      loaded: credentialsLoaded,
+    },
+
+    appUnlock: {
+      isAuthenticated,
+    },
+    navigation: {
+      cachedIsEnrolledLoaded: cachedIsEnrolledLoaded,
+      cachedIsEnrolled: cachedIsEnrolled,
+    },
+  } = state;
+
+  const irmaConfigurationLoaded = configurationLoaded && preferencesLoaded && enrollmentLoaded && credentialsLoaded;
+  const isEnrolled = enrollmentLoaded && enrolledSchemeManagerIds.length > 0;
+
+  return {
+    irmaConfigurationLoaded,
+    
+    cachedIsEnrolledLoaded,
+    cachedIsEnrolled,
+    isEnrolled,
+
+    isAuthenticated,
+  };
+};
+
+@connect(mapStateToProps)
+class RootContainer extends Component {
+
+  unsubscribeServices = null
+  componentDidMount() {
+    this.unsubscribeServices = services();
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeServices();
+  }
+
+  getNavigator() {
+    const { irmaConfigurationLoaded, cachedIsEnrolled, isEnrolled, isAuthenticated } = this.props;
+
+    // TODO: Handle ForceUpdate here as well
+
+    if (!irmaConfigurationLoaded) {
+      if (cachedIsEnrolled)
+        return AppUnlockNavigator;
+
+      return EnrollmentNavigator;
+    }
+
+    if (!isEnrolled)
+      return EnrollmentNavigator;
+
+    if (!isAuthenticated)
+      return AppUnlockNavigator;
+
+    return MainNavigator;
+  }
+
+  render() {
+    const { cachedIsEnrolledLoaded } = this.props;
+    if (!cachedIsEnrolledLoaded)
+      return null;
+
+    const navigator = this.getNavigator();
+    return <Root navigator={navigator} />;
+  }
+}
+
+class Root extends PureComponent {
+  render() {
+    const { navigator } = this.props;
+    const Navigator = createAppContainer(navigator);
+   
+    return (
+      <StyleProvider style={getTheme()}>
+        <NBRoot>
+          <Navigator />
+        </NBRoot>
+      </StyleProvider>
+    );
+  }
+}
---
+++ components/Root/navigators.js
@@ -0,0 +1,71 @@
+import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
+
+import nbVariables from 'lib/native-base-theme/variables/platform';
+
+import About from 'components/About';
+import AppUnlock from 'components/AppUnlock';
+import ChangePin from 'components/ChangePin';
+import CredentialDashboard from 'components/CredentialDashboard';
+import Enrollment from 'components/Enrollment';
+import EnrollmentTeaser from 'components/EnrollmentTeaser';
+import ForcedUpdate from 'components/ForcedUpdate';
+import PreferencesDashboard from 'components/PreferencesDashboard';
+import QRScanner from 'components/QRScanner';
+import Session from 'components/Session';
+import Sidebar from 'components/Sidebar';
+
+const defaultNavigationOptions = {
+  headerStyle: {
+    backgroundColor: nbVariables.colors.logoBlue,
+  },
+  headerTintColor: '#fff',
+  // headerTitleStyle: {
+  //   fontWeight: 'bold',
+  // },
+};
+
+const MainStack = createStackNavigator({
+  About,
+  CredentialDashboard,
+  ChangePin,
+  PreferencesDashboard,
+  QRScanner,
+  Session,
+}, {
+  initialRouteName: 'CredentialDashboard',
+  defaultNavigationOptions,
+});
+
+const MainStackWithDrawer = createDrawerNavigator({
+  CredentialDashboard: MainStack,
+}, {
+  initialRouteName: 'CredentialDashboard',
+  contentComponent: Sidebar,
+});
+
+const EnrollmentStack = createStackNavigator({
+  Enrollment: Enrollment,
+  EnrollmentTeaser: EnrollmentTeaser,
+}, {
+  initialRouteName: 'EnrollmentTeaser',
+  defaultNavigationOptions,
+});
+
+const AppUnlockStack = createStackNavigator({
+  AppUnlock,
+}, {
+  initialRouteName: 'AppUnlock',
+  defaultNavigationOptions,
+});
+
+const ForcedUpdateStack = createStackNavigator({
+  ForcedUpdate,
+}, {
+  initialRouteName: 'ForcedUpdate',
+  defaultNavigationOptions,
+});
+
+export const MainNavigator = MainStackWithDrawer;
+export const EnrollmentNavigator = EnrollmentStack;
+export const AppUnlockNavigator = AppUnlockStack;
+export const ForcedUpdateNavigator = ForcedUpdateStack;
