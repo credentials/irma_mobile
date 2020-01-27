ccb4db8213b747afbfa1b692e720660eab21fc88
Leon <leon.botros@ru.nl>
2020-01-27 10:05:31 +0100

    Remove TODO about how to handle displaying empty attributes

--- components/CredentialCard/helpers.js
+++ components/CredentialCard/helpers.js
@@ -103,10 +103,6 @@ export const CredentialAttribute = ({attribute, hasExpired = false, showDescript
   const nameStyle = {color: '#828282', fontSize: 14};
   const expiredStyle = {color: '#a7a7a7'};
 
-  // TODO: Decide how to handle empty attribute values. For now, just render them.
-  // if (attribute.Value[lang] === '')
-  //  return null;
-
   const style = {
     flex: 1,
     flexDirection: 'column',
